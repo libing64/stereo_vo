5683329c757c8a817fac7722268454c0089786e3
libing64 <libing64@gmail.com>
2020-12-08 23:24:34 +0800

    add image callback

---
+++ src/stereo_vo_node.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <Eigen/Eigen>
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include "stereo_vo.h"
+
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+using namespace std;
+using namespace Eigen;
+using namespace cv;
+
+void image_callback(const sensor_msgs::ImageConstPtr &left_image_msg,
+                    const sensor_msgs::ImageConstPtr &right_image_msg)
+{
+    Mat left_img = cv_bridge::toCvCopy(left_image_msg, string("mono8"))->image;
+    Mat right_img = cv_bridge::toCvCopy(left_image_msg, string("mono8"))->image;
+    imshow("left", left_img);
+    imshow("right", right_img);
+    waitKey(2);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "stereo_vo");
+    ros::NodeHandle n("~");
+
+    message_filters::Subscriber<sensor_msgs::Image> left_img_sub(n, "left_image", 1);
+    message_filters::Subscriber<sensor_msgs::Image> right_img_sub(n, "left_image", 2);
+
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
+    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), left_img_sub, right_img_sub);
+    sync.registerCallback(boost::bind(&image_callback, _1, _2));
+
+    ros::spin();
+    return 0;
+}
